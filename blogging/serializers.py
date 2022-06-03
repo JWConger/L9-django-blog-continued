16f41ec6267ed02cd308a81de6e62579e1929b55
JWConger <j.w.conger@gmail.com>
2022-06-02 19:34:30 -0700

    installed and set up djangorestframework, created serializers.py

---
+++ blogging/serializers.py
@@ -0,0 +1,4 @@
+from django.contrib.auth.models import User
+from models import Post, Category
+import djangorestframework
+
--- mysite/urls.py
+++ mysite/urls.py
@@ -26,4 +26,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("login/", LoginView.as_view(template_name="login.html"), name="login"),
     path("logout/", LogoutView.as_view(next_page="/"), name="logout"),
+    # path("api/", include(router.urls)),
+    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
 ]
