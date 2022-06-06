6e8b35223dcb4b12f745810c1d5b78c7fdc47f51
JWConger <j.w.conger@gmail.com>
2022-06-05 22:30:46 -0700

    completed functionality to add form, added requirement for authentication, added username to base template

--- blogging/views.py
+++ blogging/views.py
@@ -5,6 +5,15 @@ from django.views.generic.detail import DetailView
 from rest_framework import viewsets, permissions
 from blogging.serializers import UserSerializer, PostSerializer, CategorySerializer
 
+from django.shortcuts import render, redirect
+from django.utils import timezone
+from blogging.forms import add_post
+
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login
+
+# from mysite import settings
+
 
 class PostListView(ListView):
     queryset = Post.objects.exclude(published_date__exact=None).order_by(
@@ -49,3 +58,23 @@ class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all().order_by("name")
     serializer_class = CategorySerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+@login_required
+def add_model(request):
+    # if not request.user.is_authenticated:
+    #     return redirect('%s?next=%s' % (settings.LOGIN_URL, request.path))
+
+    if request.method == "POST":
+        form = add_post(request.POST)
+        if form.is_valid():
+            model_instance = form.save(commit=False)
+            model_instance.author = request.user
+            model_instance.published_date = timezone.now()
+            model_instance.save()
+            return redirect("/")
+
+    else:
+        form = add_post()
+
+        return render(request, "blogging/add.html", {"form": form})
